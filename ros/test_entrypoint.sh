e68a0438e53010526edaab8ab24c71394c19894a
Mateus Amarante <mateus.amarujo@gmail.com>
2020-08-06 18:04:49 -0300

    Fix package build + add messages

--- ros/test_entrypoint.sh
+++ ros/test_entrypoint.sh
@@ -4,20 +4,27 @@ PACKAGE=$1
 
 echo $PACKAGE
 
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
 
 rosdep init && rosdep update
 
 mkdir src/
 
+echo "<<<<<<<<<<<<<<<<<<<$PACKAGE>>>>>>>>>>>>>>>>"
+echo "<<<$PACKAGE: VCS IMPORT>>>"
 vcs import --input src.repos src/
 
-rosdep install -y --from-paths src/$PACKAGE
+echo "<<<$PACKAGE: ROSDEP INSTALL>>>"
+rosdep install --rosdistro=$ROS_DISTRO -y --from-paths src/$PACKAGE
 source /opt/ros/noetic/setup.bash
 
-colcon build --packages-select $(basename $PACKAGE)
+echo "<<<$PACKAGE: BUILD>>>"
+colcon build --packages-up-to $(basename $PACKAGE)
 
 source install/local_setup.sh
 
+echo "<<<$PACKAGE: TEST>>>"
 colcon test --packages-select $(basename $PACKAGE)
+
 colcon test-result
